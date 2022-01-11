84048a292cf75f0920674a719dd85ef35de830a6
onewoorks <irwanbibrahim@onewoorks.com>
2022-01-12 05:37:00 +0800

    update sikit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,69 @@
 <html>
 <head>
     <title>Consigment Apps</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @laravelPWA
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        
+        a { text-decoration:none; color: inherit;}
+        body { font-size: 13px,}
+        h1 { font-size: 20px; margin-bottom:0px;};
+        h3 { font-size:16px};
+        .card { border-radius : 30px};
+        .container-fluid { margin-left:0; padding-right:0};
+    </style>
 </head>
 <body>
-<a href="my.bluetoothprint.scheme://https://api.onewoorks-solutions.com/printer_test/1.php">PRINT TEST</a>
-        <br>
-        <br>
-        <a href="my.bluetoothprint.scheme://https://api.onewoorks-solutions.com/printer_test/2.php">PRINT TEST 2</a>
-        <br>
-        <br>
-        <a href="my.bluetoothprint.scheme://https://consignment-apps.onewoorks-solutions.com/resit/sales/1">PRINT TEST 2</a>
+    <div style="height:290px; background-color: orange; ">&nbsp;</div>
+        
+    <div class="container-fluid" style="margin-top: -50px;">
+        <div class="card">
+            <div class="card-body text-center">
+                <h1>CONSIGNMENT APPS DEVELOPMENT</h1>
+            </div>
+            
+        </div>
+        
+        <div class="d-grid mt-5">
+            <a 
+                class=""
+                href="/inventory">
+                <div class="card mt-2">
+                    <div class="card-body">
+                        <h3>Inventory</h3>
+                        <div>Item registered to be sold</div>
+                    </div>
+                </div>
+            </a>
+
+        <a 
+            class=""
+            href="/sales">
+            <div class="card mt-2">
+                <div class="card-body">
+                    <h3>Sales</h3>
+                    <div>List of sales reports</div>
+                </div>
+            </div>
+        </a>
+
+        <a 
+            class=""
+            href="my.bluetoothprint.scheme://https://consignment-apps.onewoorks-solutions.com/resit/sales/1">
+            <div class="card mt-2">
+            <div class="card-body">
+                <h3>Test Print</h3>
+                <div>Testing print to connected bluetooth printer</div>
+            </div>
+        </div>
+        </a>            
+        </div>
+        
+        
+        
+    </div>
+
 </body>
 </html>
